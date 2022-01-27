6e2b950ad5d32f93d310f6aedf23006b7a8a1b0b
Manuel Ferrer <cmanuferrer@gmail.com>
2022-01-27 15:11:37 -0300

    Cambios para pundit

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,20 +1,24 @@
 class TrucksController < ApplicationController
   def index
-    @trucks = Truck.all
+    @trucks = policy_scope(Truck).order(created_at: :desc)
   end
 
   def show
     @truck = Truck.find(params[:id])
+    authorize @truck
   end
 
   def new
     @truck = Truck.new
+    authorize @truck
   end
 
   def create
     @truck = Truck.new(truck_params)
     @truck.user = current_user
+    authorize @truck
     @truck.save
+
     if @truck.save
       redirect_to truck_path(@truck)
     else
@@ -24,16 +28,19 @@ class TrucksController < ApplicationController
 
   def destroy
     @truck = Truck.find(params[:id])
+    authorize @truck
     @truck.destroy
     redirect_to root_path
   end
 
   def edit
     @truck = Truck.find(params[:id])
+    authorize @truck
   end
 
   def update
     @truck = Truck.find(params[:id])
+    authorize @truck
     @truck.update(truck_params)
     redirect_to truck_path(@truck)
   end
