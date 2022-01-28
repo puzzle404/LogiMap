858b33c749fcf4004c51536ba45b98c745bed7d3
Thomas Catz <thomas@spleatapp.com>
2022-01-28 16:07:05 -0300

    cambios finales

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :trucks, only: [:index, :show, :new, :create, :edit, :update] do
     resources :rents, only: %i[new create]
   end
+  resources :rents, only: %i[index], as: 'my_rentals'
   resources :trucks, only: %i[destroy], as: 'delete_truck'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
